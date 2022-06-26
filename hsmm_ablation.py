fde5bbe5987b5cde1be1f1236785dc0bad4feb79
souljaboy764 <6441764+souljaboy764@users.noreply.github.com>
2022-06-26 23:34:11 +0200

    added plotting in hsmm ablation

--- hsmm_ablation.py
+++ hsmm_ablation.py
@@ -48,26 +48,22 @@ if __name__=='__main__':
 	nb_states = np.arange(4,15)
 	train_mse = np.zeros((len(nb_states), NUM_ACTIONS))
 	test_mse = np.zeros((len(nb_states), NUM_ACTIONS))
+	actions = ['Waving', 'Handshaking', 'Rocket Fistbump', 'Parachute Fistbump']
 	with torch.no_grad():
 		def hsmm_mse(hsmm, z_in, x2_gt):
 			z2_pred, _ = hsmm.condition(z_in, dim_in=slice(0, 2*z_dim), dim_out=slice(2*z_dim, 3*z_dim))
 			x2_gen = model._output(model._decoder(torch.Tensor(z2_pred).to(device)))
-			return F.mse_loss(x2_gt, x2_gen, reduction='mean').detach().cpu().numpy()
+			return F.mse_loss(x2_gt, x2_gen, reduction='mean').detach().cpu().numpy(), x2_gen
 		for i in range(NUM_ACTIONS):
 			print('Action',i)
 			s = train_dataset.actidx[i]
-			z_encoded_train = []
-			z_encoded_test = []
-			j_list = []
-			x2_gt_train = []
-			x2_gt_test = []
-			# for j in range(s[0], s[1]):
-			for j in range(s[0], s[1]): # Training became unstable with too many samples
-				j_list.append(j)
+			x1_gt_train, x1_gt_test, x2_gt_train, x2_gt_test, z_encoded_train, z_encoded_test = [], [], [], [], [], []
+			for j in range(s[0], s[1]): 
 				x, idx, label = train_dataset[j]
 				assert np.all(label == i)
 				x = torch.Tensor(x).to(device)
 				seq_len, dims = x.shape
+				x1_gt_train.append(x[:, :dims//2])
 				x2_gt_train.append(x[:, dims//2:])
 				x = torch.concat([x[None, :, :dims//2], x[None, :, dims//2:]]) # x[0] = Agent 1, x[1] = Agent 2
 				zpost_samples = model(x, encode_only=True)
@@ -79,9 +75,9 @@ if __name__=='__main__':
 			s = test_dataset.actidx[i]
 			for j in range(s[0], s[1]):
 				x, idx, label = test_dataset[j]
-				assert np.all(label == i)
 				x = torch.Tensor(x).to(device)
 				seq_len, dims = x.shape
+				x1_gt_test.append(x[:, :dims//2])
 				x2_gt_test.append(x[:, dims//2:])
 				zpost_samples = model(x[:, :dims//2], encode_only=True)
 				z_vel = torch.diff(zpost_samples, prepend=zpost_samples[0:1], dim=0)
@@ -97,14 +93,41 @@ if __name__=='__main__':
 
 				mse = []
 				for j in range(len(x2_gt_train)):
-					mse.append(hsmm_mse(hsmm, z_encoded_train[j][:, :2*z_dim], x2_gt_train[j]))
+					mse_j, x2_gen = hsmm_mse(hsmm, z_encoded_train[j][:, :2*z_dim], x2_gt_train[j])
+					mse.append(mse_j)
 				train_mse[n, i] = np.sum(mse)
 
 				mse = []
 				for j in range(len(x2_gt_test)):
-					mse.append(hsmm_mse(hsmm, z_encoded_test[j], x2_gt_test[j]))
+					mse_j, x2_gen = hsmm_mse(hsmm, z_encoded_test[j], x2_gt_test[j])
+					mse.append(mse_j)
 				test_mse[n, i] = np.sum(mse)
 				print('Finished',nb_state,'states',test_mse[n, i],train_mse[n, i])
+
+			x2_gen = x2_gen.reshape(-1, model.num_joints, 3).cpu().numpy()
+			x1_gt = x1_gt_test[j].reshape(-1, model.num_joints, 3).cpu().numpy()
+			x2_gt = x2_gt_test[j].reshape(-1, model.num_joints, 3).cpu().numpy()
+			fig = plt.figure()
+			ax = fig.add_subplot(1, 1, 1, projection='3d')
+			ax.view_init(20, -45)
+			plt.ion()
+			for t in range(seq_len):
+				plt.cla()
+				ax.set_xlim3d([-0.2, 0.8])
+				ax.set_ylim3d([-0.3, 0.7])
+				ax.set_zlim3d([-0.5, 0.5])
+				ax.set_title(actions[i])
+				ax.plot(x1_gt[t, :, 2], -x1_gt[t, :, 0], x1_gt[t, :, 1], color='k', marker='o', markerfacecolor='r', label='Agent 1 GT')
+				ax.plot(0.65-x2_gt[t, :, 2], 0.3+x2_gt[t, :, 0], x2_gt[t, :, 1], color='k', marker='o', markerfacecolor='g', label='Agent 2 GT')
+				ax.plot(0.65-x2_gen[t, :, 2], 0.3+x2_gen[t, :, 0], x2_gen[t, :, 1], color='k', marker='o', markerfacecolor='b', linestyle='--', label='Agent 2 Pred')
+				plt.legend()
+				plt.draw()
+				plt.pause(0.03)
+				if not plt.fignum_exists(fig.number):
+					break
+			plt.ioff()
+			# plt.close("all")
+			plt.show()
 
 	fig = plt.figure()
 	ax = fig.add_subplot(1, 2, 1)
@@ -113,6 +136,7 @@ if __name__=='__main__':
 	ax = fig.add_subplot(1, 2, 2)
 	ax.imshow(test_mse)
 	ax.set_title('Testing MSE')
+	plt.show()
 	plt.savefig('hsmm_ablation.png')
 	print(train_mse)
 	print(test_mse)
