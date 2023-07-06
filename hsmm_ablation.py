8e81cf5e21f60be19513091f236fa9c7fb390673
Vignesh Prasad <vignesh.prasad@tu-darmstadt.de>
2023-07-06 14:59:47 +0200

    some more ablative experiments

--- hsmm_ablation.py
+++ hsmm_ablation.py
@@ -1,146 +1,134 @@
 import torch
 import torch.nn.functional as F
-
+from torch.utils.data import DataLoader
 import numpy as np
-import os, argparse
-import matplotlib.pyplot as plt
-from joblib import Parallel, delayed
-
-os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
-
-import networks
-import dataloaders
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 import pbdlib as pbd
 
-if __name__=='__main__':
-	parser = argparse.ArgumentParser(description='SKID Training')
-	parser.add_argument('--ckpt', type=str, metavar='CKPT', required=True,
-						help='Checkpoint to test')
-	args = parser.parse_args()
-	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-	torch.manual_seed(128542)
-	torch.autograd.set_detect_anomaly(True)
+from datetime import datetime
 
-	MODELS_FOLDER = os.path.dirname(args.ckpt)
-	hyperparams = np.load(os.path.join(MODELS_FOLDER,'hyperparams.npz'), allow_pickle=True)
-	saved_args = hyperparams['args'].item() # overwrite args if loading from checkpoint
-	# vae_config = getattr(config, saved_args.dataset).ae_config()
-	vae_config = hyperparams['ae_config'].item()
+import networks
+import config
+from utils import *
+import dataloaders
 
-	print("Creating Model")
-	model = getattr(networks, saved_args.model)(**(vae_config.__dict__)).to(device)
-	z_dim = model.latent_dim
+torch.autograd.set_detect_anomaly(True)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_types = "ablation_vae"
+z_dims = [10]
+z_trajs = []
+z_trajs_cov = []
+
+z_dim = 3
+ckpt = f'logs/2023/buetepage_downsampled/mild_sophia_ablation_5hsmm/z3/trial0/models/final.pth' # input()
+dirname = os.path.dirname(ckpt)
+hyperparams = np.load(os.path.join(dirname,'hyperparams.npz'), allow_pickle=True)
+args = hyperparams['args'].item()
+ckpt = torch.load(ckpt)
+
+model = getattr(networks, args.model)(**(hyperparams['ae_config'].item().__dict__)).to(device)
+model.load_state_dict(ckpt['model'])
+model.eval()
+z_dim = 3
+nb_dim = 4*z_dim
+# dataset = getattr(dataloaders, args.dataset)
+train_iterator = dataloaders.buetepage_downsampled.SequenceWindowDataset('./data/buetepage/traj_data.npz', train=True, window_length=40)
+test_iterator = dataloaders.buetepage_downsampled.SequenceWindowDataset('./data/buetepage/traj_data.npz', train=False, window_length=40)
+# for a in range(len(train_iterator.actidx)):
+# 	s = train_iterator.actidx[a]
+# 	z_encoded = []
+# 	z_encoded_cov = []
+# 	for j in range(s[0], s[1]):
+# 	# for j in np.random.randint(s[0], s[1], 12):
+# 		x, label = train_iterator[j]
+# 		assert np.all(label == a)
+# 		x = torch.Tensor(x).to(device)
+# 		_, seq_len, dims = x.shape
+# 		# x = torch.concat([x[None, :, :dims//2], x[None, :, dims//2:]]) # x[0] = Agent 1, x[1] = Agent 2
+		
+# 		# zpost_samples = model(x, encode_only=True)
+# 		# if model.window_size == 1:
+# 		# 	z1_vel = torch.diff(zpost_samples[0], prepend=zpost_samples[0][0:1], dim=0)
+# 		# 	z2_vel = torch.diff(zpost_samples[1], prepend=zpost_samples[1][0:1], dim=0)
+# 		# 	z_encoded.append(torch.concat([zpost_samples[0], z1_vel, zpost_samples[1], z2_vel], dim=-1).detach().cpu().numpy()) # (num_trajs, seq_len, 2*z_dim)
+# 		# else:
+# 		# 	z_encoded.append(torch.concat([zpost_samples[0], zpost_samples[1]], dim=-1).detach().cpu().numpy()) # (num_trajs, seq_len, 2*z_dim)
+
+# 		x_gen, zpost_samples, zpost_dist = model(x)
+# 		z_encoded.append(torch.concat([zpost_dist.mean[0], zpost_dist.mean[1]], dim=-1).detach().cpu().numpy()) # (num_trajs, seq_len, 2*z_dim)
+# 		z_encoded_cov.append(torch.concat([zpost_dist.covariance_matrix[0], zpost_dist.covariance_matrix[1]], dim=-1).detach().cpu().numpy()) # (num_trajs, seq_len, 2*z_dim)
+# 	z_encoded = np.array(z_encoded)
+# 	z_encoded_cov = np.array(z_encoded_cov)
+# 	z_trajs.append(z_encoded)
+# 	z_trajs_cov.append(z_encoded_cov)
+
+# np.savez_compressed('z_trajs.npz', mu=z_trajs, covs=z_trajs_cov)
+
+data = np.load('z_trajs.npz', allow_pickle=True)
+z_trajs = data['mu']
+z_trajs_cov = data['covs']
+nb_states = 6
+num_train = 5
+
+print('Conditioning with Cov')
+for nb_states in [4,5,6,10,12]:
+	hsmm = [pbd.HSMM(nb_dim=nb_dim, nb_states=nb_states) for a in range(len(train_iterator.actidx))]
 	
-	print("Loading Checkpoints")
-	ckpt = torch.load(args.ckpt)
-	model.load_state_dict(ckpt['model'])
-	model.eval()
-	
-	print("Reading Data")
-	train_dataset = getattr(dataloaders, saved_args.dataset).SequenceDataset(saved_args.src, train=True)
-	test_dataset = getattr(dataloaders, saved_args.dataset).SequenceDataset(saved_args.src, train=True)
-	NUM_ACTIONS = len(train_dataset.actidx)
-
-	print("Starting")
-	reconstruction_error, gt_data, gen_data = [], [], []
-	hsmm_list = []
-	nb_dim = 2*model.latent_dim
-	nb_states = np.arange(8,9)
-	train_mse = np.zeros((len(nb_states), NUM_ACTIONS))
-	test_mse = np.zeros((len(nb_states), NUM_ACTIONS))
-	actions = ['Waving', 'Handshaking', 'Rocket Fistbump', 'Parachute Fistbump']
-	with torch.no_grad():
-		def hsmm_mse(hsmm, z_in, x2_gt):
-			z2_pred, _ = hsmm.condition(z_in, dim_in=slice(0, 2*z_dim), dim_out=slice(2*z_dim, 3*z_dim))
-			x1_gen = model._output(model._decoder(torch.Tensor(z_in[:, :z_dim]).to(device)))
-			x2_gen = model._output(model._decoder(torch.Tensor(z2_pred).to(device)))
-			return F.mse_loss(x2_gt, x2_gen, reduction='mean').detach().cpu().numpy(), x1_gen, x2_gen
-		for i in range(NUM_ACTIONS):
-			print('Action',i)
-			s = train_dataset.actidx[i]
-			x1_gt_train, x1_gt_test, x2_gt_train, x2_gt_test, z_encoded_train, z_encoded_test = [], [], [], [], [], []
-			for j in range(s[0], s[1]): 
-				x, idx, label = train_dataset[j]
-				assert np.all(label == i)
-				x = torch.Tensor(x).to(device)
-				seq_len, dims = x.shape
-				x1_gt_train.append(x[:, :dims//2])
-				x2_gt_train.append(x[:, dims//2:])
-				x = torch.concat([x[None, :, :dims//2], x[None, :, dims//2:]]) # x[0] = Agent 1, x[1] = Agent 2
-				zpost_samples = model(x, encode_only=True)
-
-				z1_vel = torch.diff(zpost_samples[0], prepend=zpost_samples[0][0:1], dim=0)
-				z2_vel = torch.diff(zpost_samples[1], prepend=zpost_samples[1][0:1], dim=0)
-				z_encoded_train.append(torch.concat([zpost_samples[0], z1_vel, zpost_samples[1], z2_vel], dim=-1).cpu().numpy()) # (num_trajs, seq_len, 2*z_dim)
+	mse_error = []
+	mse_error_cov = []
+	for a in range(len(train_iterator.actidx)):
+		hsmm[a].init_hmm_kbins(z_trajs[a])
+		hsmm[a].em(z_trajs[a], nb_max_steps=100)
+		
+		for i in range(len(z_trajs[a])):
+			z_traj = np.array(z_trajs[a][i])
+			z_traj_cov = np.array(z_trajs_cov[a][i])
+			z2_pred, _ = hsmm[a].condition(z_traj[:, :z_dim], z_traj_cov[:, :, :z_dim], dim_in=slice(0, z_dim), dim_out=slice(z_dim, 2*z_dim))
+			z1_pred, _ = hsmm[a].condition(z_traj[:, z_dim:], z_traj_cov[:, :, z_dim:], dim_in=slice(z_dim, 2*z_dim), dim_out=slice(0, z_dim))
 			
-			s = test_dataset.actidx[i]
-			for j in range(s[0], s[1]):
-				x, idx, label = test_dataset[j]
-				x = torch.Tensor(x).to(device)
-				seq_len, dims = x.shape
-				x1_gt_test.append(x[:, :dims//2])
-				x2_gt_test.append(x[:, dims//2:])
-				zpost_samples = model(x[:, :dims//2], encode_only=True)
-				z_vel = torch.diff(zpost_samples, prepend=zpost_samples[0:1], dim=0)
-				z = torch.concat([zpost_samples,z_vel], dim=-1)
-				z_encoded_test.append(z.cpu().numpy()) # (num_trajs, seq_len, 2*z_dim)
+			mse_i = ((z_traj[:, z_dim:] - z2_pred)**2).sum(-1)
+			mse_error_cov += mse_i.tolist()
+
+			z2_pred, _ = hsmm[a].condition(z_traj[:, :z_dim], None, dim_in=slice(0, z_dim), dim_out=slice(z_dim, 2*z_dim))
+			z1_pred, _ = hsmm[a].condition(z_traj[:, z_dim:], None, dim_in=slice(z_dim, 2*z_dim), dim_out=slice(0, z_dim))
 			
-			for n in range(len(nb_states)):
-				nb_state = nb_states[n]
-				print('Running',nb_state,'states')
-				hsmm = pbd.HSMM(nb_dim=nb_dim, nb_states=nb_state)
-				hsmm.init_hmm_kbins(z_encoded_train)
-				hsmm.em(z_encoded_train)
+			mse_i = ((z_traj[:, z_dim:] - z2_pred)**2).sum(-1)
+			mse_error += mse_i.tolist()
+			# recon_error += recon_i.tolist()
+	print(f"| W/o Cov | {a} | {nb_states} | {np.mean(mse_error):.4e} ± {np.std(mse_error):.4e} |")
+	print(f"| W/  Cov | {a} | {nb_states} | {np.mean(mse_error_cov):.4e} ± {np.std(mse_error_cov):.4e} |")
+			# print(f'MSE: {np.sum(mse_error)}')
+	print('')
 
-				mse = []
-				for j in range(len(x2_gt_train)):
-					mse_j, x1_gen, x2_gen = hsmm_mse(hsmm, z_encoded_train[j][:, :2*z_dim], x2_gt_train[j])
-					mse.append(mse_j)
-				train_mse[n, i] = np.sum(mse)
-
-				mse = []
-				for j in range(len(x2_gt_test)):
-					mse_j, x1_gen,  x2_gen = hsmm_mse(hsmm, z_encoded_test[j], x2_gt_test[j])
-					mse.append(mse_j)
-				test_mse[n, i] = np.sum(mse)
-				print('Finished',nb_state,'states',test_mse[n, i],train_mse[n, i])
-
-			x1_gen = x1_gen.reshape(-1, model.num_joints, 3).cpu().numpy()
-			x2_gen = x2_gen.reshape(-1, model.num_joints, 3).cpu().numpy()
-			x1_gt = x1_gt_test[j].reshape(-1, model.num_joints, 3).cpu().numpy()
-			x2_gt = x2_gt_test[j].reshape(-1, model.num_joints, 3).cpu().numpy()
-			np.savez_compressed('predictions_action_'+str(i), x1_gt=x1_gt, x2_gt=x2_gt, x1_gen=x1_gen, x2_gen=x2_gen)
-	# 		fig = plt.figure()
-	# 		ax = fig.add_subplot(1, 1, 1, projection='3d')
-	# 		ax.view_init(20, -45)
-	# 		plt.ion()
-	# 		for t in range(seq_len):
-	# 			plt.cla()
-	# 			ax.set_xlim3d([-0.2, 0.8])
-	# 			ax.set_ylim3d([-0.3, 0.7])
-	# 			ax.set_zlim3d([-0.5, 0.5])
-	# 			ax.set_title(actions[i])
-	# 			ax.plot(x1_gt[t, :, 2], -x1_gt[t, :, 0], x1_gt[t, :, 1], color='k', marker='o', markerfacecolor='r', label='Agent 1 GT')
-	# 			ax.plot(0.65-x2_gt[t, :, 2], 0.3+x2_gt[t, :, 0], x2_gt[t, :, 1], color='k', marker='o', markerfacecolor='g', label='Agent 2 GT')
-	# 			ax.plot(0.65-x2_gen[t, :, 2], 0.3+x2_gen[t, :, 0], x2_gen[t, :, 1], color='k', marker='o', markerfacecolor='b', linestyle='--', label='Agent 2 Pred')
-	# 			plt.legend()
-	# 			plt.draw()
-	# 			plt.pause(0.03)
-	# 			if not plt.fignum_exists(fig.number):
-	# 				break
-	# 		plt.ioff()
-	# 		# plt.close("all")
-	# 		plt.show()
-
-	# fig = plt.figure()
-	# ax = fig.add_subplot(1, 2, 1)
-	# ax.imshow(train_mse)
-	# ax.set_title('Training MSE')
-	# ax = fig.add_subplot(1, 2, 2)
-	# ax.imshow(test_mse)
-	# ax.set_title('Testing MSE')
-	# plt.show()
-	# plt.savefig('hsmm_ablation.png')
-	# print(train_mse)
-	# print(test_mse)
+# for i in range(len(z_trajs)):
+# 	for j in range(len(z_trajs[i])):
+# 		z_vel = np.diff(z_trajs[i][j], axis=0, prepend=z_trajs[i][j][0:1])
+# 		z_trajs[i][j] = np.concatenate([z_trajs[i][j][:, :z_dim], z_vel[:, :z_dim], z_trajs[i][j][:, z_dim:], z_vel[:, z_dim:]], axis=1)
+# print('with velocity')
+# for nb_states in [4,5,6,10,12]:
+# 	hsmm = [pbd.HSMM(nb_dim=nb_dim, nb_states=nb_states) for a in range(len(train_iterator.actidx))]
 	
+# 	for a in range(len(train_iterator.actidx)):
+# 		# train_idx = np.random.choice(np.arange(len(z_trajs[a])),num_train,False).astype(int)
+# 		z_trajs_train = []
+# 		for z_traj in z_trajs[a]:
+# 			z_trajs_train.append(z_traj[::5])
+# 		train_idx = np.arange(len(z_trajs[a])).astype(int)
+# 		hsmm[a].init_hmm_kbins(z_trajs_train)
+# 		hsmm[a].em(z_trajs_train, nb_max_steps=100)
+	
+# 		mse_error = []
+# 		for z_traj in z_trajs[a]:
+# 			z_traj = np.array(z_traj)
+# 			z2_pred, sigma2 = hsmm[a].condition(z_traj[:, :2*z_dim], None, dim_in=slice(0, 2*z_dim), dim_out=slice(2*z_dim, 3*z_dim))
+# 			# z2 = hsmm[label].condition(zpost_dist.mean[0].detach().cpu().numpy(), dim_in=slice(0, z_dim), dim_out=slice(z_dim, 2*z_dim))
+			
+# 			mse_i = ((z_traj[:, 2*z_dim:3*z_dim] - z2_pred)**2).sum(-1)
+# 			mse_error += mse_i.tolist()
+# 			# recon_error += recon_i.tolist()
+# 		print(f"| {a} | {nb_states} | {np.mean(mse_error):.4e} ± {np.std(mse_error):.4e} |")
+# 			# print(f'MSE: {np.sum(mse_error)}')
+# 	print('')
+
