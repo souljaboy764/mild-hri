2e886c0d0fda2fc1d0ba662c98c11e6fc2cc7985
Vignesh Prasad <vignesh.prasad@tu-darmstadt.de>
2023-07-12 19:59:57 +0200

    customizing test checkpoints

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 
 import numpy as np
 import os, datetime, argparse
-# os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
 import networks
@@ -23,17 +23,30 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # args = parser.parse_args()
 # torch.autograd.set_detect_anomaly(True)
 
-for model_type in ['mild_crossrecon_covcond', 'mild_crossrecon_nocovcond', 'mild_vanilla', 'vae_crossrecon_covcond', 'vae_crossrecon_nocovcond', 'vae_vanilla']:
+for model_type, use_cov, ckpt_name in [
+					('mild_vanilla', False, 'final.pth'),
+					('mild_vanilla', False, 'final_250.pth'),
+					('mild_vanilla', False, 'final_100_finetuning.pth'),
+					# ('mild_crossrecon_nocovcond', False, 'final_250.pth'),
+					# ('mild_crossrecon_covcond', True, 'final.pth'),
+					# ('mild_crossrecon_condsampling', True, 'final.pth'),
+					# ('mild_crossrecon_samplecond', False, 'final.pth'),
+					# ('vae_vanilla', False, 'final.pth'),
+					# ('vae_crossrecon_nocovcond', False, 'final.pth'),
+					# ('vae_crossrecon_covcond', True, 'final.pth'),
+					# ('vae_crossrecon_condsampling', True, 'final.pth'),
+					# ('vae_crossrecon_samplecond', False, 'final.pth'),
+					]:
 	pred_mse = []
+	vae_mse = []
 	for trial in range(4):
-		ckpt_path = f'logs/2023/downsampled/bp_pepper/{model_type}/z5/trial{trial}/models/final.pth'
+		ckpt_path = f'logs/2023/downsampled/bp_pepper/{model_type}/z5/trial{trial}/models/{ckpt_name}'
 		ckpt = torch.load(ckpt_path)
 		hyperparams = np.load(os.path.join(os.path.dirname(ckpt_path),'hyperparams.npz'), allow_pickle=True)
 		args_ckpt = hyperparams['args'].item()
 		global_config = hyperparams['global_config'].item()
 		ae_config = hyperparams['ae_config'].item()
 		robot_vae_config = hyperparams['robot_vae_config'].item()
-
 		if args_ckpt.dataset == 'buetepage_pepper':
 			dataset = dataloaders.buetepage.PepperWindowDataset
 		elif args_ckpt.dataset == 'buetepage':
@@ -49,10 +62,11 @@ for model_type in ['mild_crossrecon_covcond', 'mild_crossrecon_nocovcond', 'mild
 		model_r.load_state_dict(ckpt['model_r'])
 		model_r.eval()
 		hsmm = ckpt['hsmm']
+		# print(ckpt['epoch'])
 		with torch.no_grad():
 			for i, x in enumerate(test_iterator):
-				if i<7:
-					continue
+				# if i<7:
+				# 	continue
 				x, label = x
 				x = x[0]
 				label = label[0]
@@ -62,10 +76,16 @@ for model_type in ['mild_crossrecon_covcond', 'mild_crossrecon_nocovcond', 'mild
 				z_dim = hsmm[label].nb_dim//2
 				
 				zh_post = model_h(x_h, dist_only=True)
-				zr_cond, zr_cond_sigma = hsmm[label].condition(zh_post.mean, dim_in=slice(0, z_dim), dim_out=slice(z_dim, 2*z_dim), data_Sigma_in=zh_post.covariance_matrix,
-												return_cov=True)#, 
-				torch.linalg.cholesky(zr_cond_sigma)
+				xr_gen, _, _ = model_r(x_r)
+				if use_cov:
+					data_Sigma_in = zh_post.covariance_matrix
+				else: 
+					data_Sigma_in = None
+				zr_cond = hsmm[label].condition(zh_post.mean, dim_in=slice(0, z_dim), dim_out=slice(z_dim, 2*z_dim), 
+						   						data_Sigma_in=data_Sigma_in,
+												return_cov=False) 
 				xr_cond = model_r._output(model_r._decoder(zr_cond))
-				pred_mse += ((xr_cond - x_r)**2).mean(-1).detach().cpu().numpy().tolist()
+				pred_mse += ((xr_cond - x_r)**2).reshape((x_r.shape[0], model_r.window_size, model_r.num_joints)).sum(-1).mean(-1).detach().cpu().numpy().tolist()
+				vae_mse += ((xr_gen - x_r)**2).reshape((x_r.shape[0], model_r.window_size, model_r.num_joints)).sum(-1).mean(-1).detach().cpu().numpy().tolist()
 			
-	print(f'{model_type}\t{np.mean(pred_mse):.4e} ± {np.std(pred_mse):.4e}')
+	print(f'{model_type} {use_cov} \t{np.mean(pred_mse):.4e} ± {np.std(pred_mse):.4e} \t{np.mean(vae_mse):.4e} ± {np.std(vae_mse):.4e}')
--- test_cov.md
+++ test_cov.md
@@ -2,15 +2,26 @@
 
 ## without hand waving
 
-Model                       MSE                         Reconstruction                              Input cov during Conditioning
-mild_vanilla                3.3662e-02 ± 4.8302e-02     AE                                          only HSMM
-mild_vanilla with covcond   3.4058e-02 ± 4.8561e-02     AE                                          HSMM + Full
-mild_vanilla_onlycovcond    3.3608e-02 ± 4.7621e-02     AE                                          only Full
-mild_crossrecon_nocovcond   2.0180e-02 ± 3.4425e-02     AE and HSMM cov conditioned                 only HSMM
-mild_crossrecon_covcond     2.3818e-02 ± 4.0188e-02     AE and HSMM cov + Full cov conditioned      HSMM + Full
+Model                       MSE                         Reconstruction                                              Input cov during Conditioning
+mild_vanilla                3.4058e-02 ± 4.8561e-02     posterior mean                                              only HSMM
+mild_vanilla with covcond   3.4058e-02 ± 4.8561e-02     posterior mean                                              HSMM + Full
+mild_vanilla_onlycovcond    3.3608e-02 ± 4.7621e-02     posterior mean                                              only Full
+mild_crossrecon_nocovcond   2.2037e-02 ± 3.5099e-02     posterior mean and HSMM cov conditioned mean                only HSMM
+mild_crossrecon_covcond     2.4946e-02 ± 3.8571e-02     posterior mean and HSMM cov + Full cov conditioned mean     HSMM + Full
 
-vae_vanilla                 3.4147e-02 ± 4.8365e-02     AE                                          HSMM
-vae_vanilla with covcond    3.5461e-02 ± 5.0352e-02     AE                                          HSMM + Diag
-vae_vanilla_onlycovcond     3.5032e-02 ± 4.9129e-02     AE                                          only Diag
-vae_crossrecon_nocovcond    2.0811e-02 ± 3.1877e-02     AE and HSMM cov conditioned                 only HSMM
-vae_crossrecon_covcond      2.1926e-02 ± 3.1960e-02     AE and HSMM cov + Diag cov conditioned      HSMM + Diag
+vae_vanilla                 3.4147e-02 ± 4.8365e-02     posterior mean                                              HSMM
+vae_vanilla with covcond    3.5461e-02 ± 5.0352e-02     posterior mean                                              HSMM + Diag
+vae_vanilla_onlycovcond     3.5032e-02 ± 4.9129e-02     posterior mean                                              only Diag
+vae_crossrecon_nocovcond    2.0811e-02 ± 3.1877e-02     posterior mean and HSMM cov conditioned mean                only HSMM
+vae_crossrecon_covcond      2.1926e-02 ± 3.1960e-02     posterior mean and HSMM cov + Diag cov conditioned mean     HSMM + Diag
+
+
+
+mild_vanilla False      1.7696e-01 ± 2.4886e-01         3.8383e-02 ± 5.9870e-02
+mild_crossrecon_nocovcond False         1.2509e-01 ± 1.6022e-01         7.4698e-02 ± 1.0581e-01
+mild_crossrecon_covcond True    1.3258e-01 ± 1.6776e-01         7.9680e-02 ± 1.2056e-01
+mild_crossrecon_samplecond False        1.0734e-01 ± 1.6924e-01         5.6105e-02 ± 8.5978e-02
+vae_vanilla False       1.8111e-01 ± 2.4859e-01         4.5384e-02 ± 6.7237e-02
+vae_crossrecon_nocovcond False  1.0470e-01 ± 1.3238e-01         5.0419e-02 ± 7.1895e-02
+vae_crossrecon_covcond True     1.1582e-01 ± 1.3811e-01         5.5158e-02 ± 7.5922e-02
+vae_crossrecon_samplecond False         1.0625e-01 ± 1.6096e-01         4.9654e-02 ± 6.8719e-02
