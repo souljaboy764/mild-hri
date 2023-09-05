ed0487d5e13e0f8b99c84412e6eb053467b3241a
Vignesh Prasad <vignesh.prasad@tu-darmstadt.de>
2023-09-06 00:59:12 +0200

    aded both humans to data

--- mild_hri/dataloaders/buetepage.py
+++ mild_hri/dataloaders/buetepage.py
@@ -9,32 +9,35 @@ class HHDataset(Dataset):
 		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 		with np.load(datafile, allow_pickle=True) as data:
 			if train:
-				self.traj_data = data['train_data']
-				self.labels = data['train_labels']
-				self.actidx = np.array([[0,24],[24,54],[54,110],[110,149]])
+				traj_data = data['train_data']
+				labels = data['train_labels']
+				self.actidx = np.array([[0,24],[24,54],[54,110],[110,149]])*2
 
 			else:
-				self.traj_data = data['test_data']
-				self.labels = data['test_labels']
-				self.actidx = np.array([[0,7],[7,15],[15,29],[29,39]])
+				traj_data = data['test_data']
+				labels = data['test_labels']
+				self.actidx = np.array([[0,7],[7,15],[15,29],[29,39]])*2
 
-			for i in range(len(self.traj_data)):
-				self.traj_data[i] = self.traj_data[i][:, 1:, :] # Ignoring the first shoulder/body joint as it is almost static
-				seq_len, njoints, dims = self.traj_data[i].shape
-				traj_1 = self.traj_data[i][..., :3].reshape((seq_len, (njoints)*3))
-				traj_2 = self.traj_data[i][..., 3:].reshape((seq_len, (njoints)*3))
+			self.traj_data = []
+			for i in range(len(traj_data)):
+				traj_data[i] = traj_data[i][:, 1:, :] # Ignoring the first shoulder/body joint as it is almost static
+				seq_len, njoints, dims = traj_data[i].shape
+				traj_1 = traj_data[i][..., :3].reshape((seq_len, (njoints)*3))
+				traj_2 = traj_data[i][..., 3:].reshape((seq_len, (njoints)*3))
+
+				if downsample < 1:
+					traj = np.array(downsample_trajs([np.concatenate([traj_1[:, None], traj_2[:, None]], axis=-1)], int(downsample*seq_len), device))[0, :, 0, :]
+					seq_len, dims = traj.shape
+					traj_1 = traj[:, :dims//2]
+					traj_2 = traj[:, dims//2:]
 
 				vel_1 = np.diff(traj_1, axis=0, prepend=traj_1[0:1,:])
 				vel_2 = np.diff(traj_2, axis=0, prepend=traj_2[0:1,:])
+				# traj_1 = np.concatenate([traj_1, vel_1],axis=-1)
+				# traj_2 = np.concatenate([traj_2, vel_2],axis=-1)
 
-				traj_1 = np.concatenate([traj_1, vel_1],axis=-1)
-				traj_2 = np.concatenate([traj_2, vel_2],axis=-1)
-
-				if downsample < 1:
-					assert downsample != 0
-					self.traj_data[i] = np.array(downsample_trajs([np.concatenate([traj_1[:, None], traj_2[:, None]], axis=-1)], int(downsample*seq_len), device))[0, :, 0, :]
-				else:
-					self.traj_data[i] = np.concatenate([traj_1, traj_2], axis=-1)
+				self.traj_data.append(np.concatenate([traj_1, vel_1, traj_2, vel_2], axis=-1))
+				self.traj_data.append(np.concatenate([traj_2, vel_2, traj_1, vel_1], axis=-1))
 			
 			self.len = len(self.traj_data)
 			self.labels = np.zeros(self.len)
@@ -66,20 +69,19 @@ class PepperDataset(HHDataset):
 	def __init__(self, datafile, train=True, downsample=1):
 		super(PepperDataset, self).__init__(datafile, train, downsample)
 		
-		self.joints_min = np.ones(4)*10
-		self.joints_max = np.ones(4)*-10
 		for i in range(len(self.traj_data)):
 			seq_len, dims = self.traj_data[i].shape
+			# input_slice = slice(int(2*dims/3),dims)
+			input_slice = slice(int(dims//2), int(3*dims//4))
 			traj_r = []
 
-			# for frame in self.traj_data[i][:, dims//2:].reshape((seq_len, dims//6, 3)):
-			for frame in self.traj_data[i][:, dims//2:].reshape((seq_len, -1, 3)):
+			for frame in self.traj_data[i][:, input_slice].reshape((seq_len, -1, 3)):
 				joints = joint_angle_extraction(frame)
 				traj_r.append(joints)
 
 			traj_r = np.array(traj_r) # seq_len, 4
 			
-			self.traj_data[i] = np.concatenate([self.traj_data[i][:, :dims//2], traj_r], axis=-1) # seq_len, dims//2 + 4
+			self.traj_data[i] = np.concatenate([self.traj_data[i][:, :input_slice.start], traj_r], axis=-1) # seq_len, input_dim + 4
 	
 class PepperWindowDataset(HHWindowDataset):
 	def __init__(self, datafile, train=True, window_length=40, downsample = 1):
@@ -88,8 +90,6 @@ class PepperWindowDataset(HHWindowDataset):
 		self.traj_data = window_concat(self._dataset.traj_data, window_length, 'pepper')
 		self.len = len(self.traj_data)
 		self.labels = np.zeros(self.len)
-		self.joints_max = np.tile(self._dataset.joints_max, window_length)
-		self.joints_min = np.tile(self._dataset.joints_min, window_length)
 		for idx in range(len(self.actidx)):
 			self.labels[self.actidx[idx][0]:self.actidx[idx][1]] = idx
 
--- mild_hri/dataloaders/nuisi.py
+++ mild_hri/dataloaders/nuisi.py
@@ -9,35 +9,33 @@ class HHDataset(Dataset):
 		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 		with np.load(datafile, allow_pickle=True) as data:
 			if train:
-				self.traj_data = data['train_data']
-				self.labels = data['train_labels']
-				self.actidx = np.array([[0, 9], [9, 17], [17, 26], [26, 33]])
+				traj_data = data['train_data']
+				labels = data['train_labels']
+				self.actidx = np.array([[0, 9], [9, 17], [17, 26], [26, 33]])*2
 
 			else:
-				self.traj_data = data['test_data']
-				self.labels = data['test_labels']
-				self.actidx = np.array([[0, 3], [3, 6], [6, 9], [9, 11]])
+				traj_data = data['test_data']
+				labels = data['test_labels']
+				self.actidx = np.array([[0, 3], [3, 6], [6, 9], [9, 11]])*2
 			
-			self.len = len(self.traj_data)
-			self.labels = np.zeros(self.len)
-			for idx in range(len(self.actidx)):
-				self.labels[self.actidx[idx][0]:self.actidx[idx][1]] = idx
-
-			for i in range(len(self.traj_data)):
-				seq_len, dims = self.traj_data[i].shape
-				traj_1 = self.traj_data[i][:, :dims//2]
-				traj_2 = self.traj_data[i][:, dims//2:]
+			self.traj_data = []
+			for i in range(len(traj_data)):
+				seq_len, dims = traj_data[i].shape
+				traj_1 = traj_data[i][:, :dims//2]
+				traj_2 = traj_data[i][:, dims//2:]
 				# traj_1 = self.traj_data[i][:, dims//2-3:dims//2]
 				# traj_2 = self.traj_data[i][:, -3:]
 
 				vel_1 = np.diff(traj_1, axis=0, prepend=traj_1[0:1,:])
 				vel_2 = np.diff(traj_2, axis=0, prepend=traj_2[0:1,:])
 
-				traj_1 = np.concatenate([traj_1, vel_1],axis=-1)
-				traj_2 = np.concatenate([traj_2, vel_2],axis=-1)
-
-				self.traj_data[i] = np.concatenate([traj_1, traj_2], axis=-1)
+				self.traj_data.append(np.concatenate([traj_1, vel_1, traj_2, vel_2], axis=-1))
+				self.traj_data.append(np.concatenate([traj_2, vel_2, traj_1, vel_1], axis=-1))
 			
+			self.len = len(self.traj_data)
+			self.labels = np.zeros(self.len)
+			for idx in range(len(self.actidx)):
+				self.labels[self.actidx[idx][0]:self.actidx[idx][1]] = idx
 
 	def __len__(self):
 		return self.len
@@ -64,30 +62,21 @@ class HHWindowDataset(Dataset):
 	
 class PepperDataset(HHDataset):
 	def __init__(self, datafile, train=True, downsample=None):
-		super(PepperDataset, self).__init__(datafile, train, downsample)
+		super().__init__(datafile, train, downsample)
 		
-		self.joints_min = np.ones(4)*10
-		self.joints_max = np.ones(4)*-10
 		for i in range(len(self.traj_data)):
 			seq_len, dims = self.traj_data[i].shape
 			traj_r = []
 
-			for frame in self.traj_data[i][:, dims//2:].reshape((seq_len, dims//6, 3)):
+			for frame in self.traj_data[i][:, dims//2:3*dims//4].reshape((seq_len, 3, 3)):
 				joints = joint_angle_extraction(frame)
 				traj_r.append(joints)
 
 			traj_r = np.array(traj_r) # seq_len, 4
 
-			traj_min = traj_r.min(0)
-			traj_max = traj_r.max(0) 
-			self.joints_min = np.where(self.joints_min>traj_min, traj_min, self.joints_min)
-			self.joints_max = np.where(self.joints_max<traj_max, traj_max, self.joints_max)
-			
 			self.traj_data[i] = np.concatenate([self.traj_data[i][:, :dims//2], traj_r], axis=-1) # seq_len, dims//2 + 4
 			# self.traj_data[i] = np.concatenate([self.traj_data[i][:, dims//4-3:dims//4], self.traj_data[i][:, dims//2-3:dims//2], traj_r], axis=-1) # seq_len, dims//2 + 4
-		# print(self.joints_min)
-		# print(self.joints_max)
-	
+		
 class PepperWindowDataset(HHWindowDataset):
 	def __init__(self, datafile, train=True, window_length=5, downsample = 1):
 		self._dataset = PepperDataset(datafile, train, downsample)
@@ -95,8 +84,6 @@ class PepperWindowDataset(HHWindowDataset):
 		self.traj_data = window_concat(self._dataset.traj_data, window_length, 'pepper')
 		self.len = len(self.traj_data)
 		self.labels = np.zeros(self.len)
-		self.joints_max = np.tile(self._dataset.joints_max, window_length)
-		self.joints_min = np.tile(self._dataset.joints_min, window_length)
 		for idx in range(len(self.actidx)):
 			self.labels[self.actidx[idx][0]:self.actidx[idx][1]] = idx
 
